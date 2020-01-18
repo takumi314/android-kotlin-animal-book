9e72ef3b4b3f3f4cf3570288353089dbdb2cbd23
takumi314 <isospin_symmetry@me.com>
2020-01-19 03:01:43 +0900

    add control fragment programmatically

--- app/src/main/java/com/example/animalbook/SubActivity.kt
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -2,6 +2,7 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
 
@@ -9,4 +10,44 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
     }
+
+    private fun setOnClickListener() {
+        // 「ライオン」ボタンのタップイベントを監視する
+        lionButton.setOnClickListener {
+            val fragment = LionFragment()
+            // AppCompatActivityクラスのメソッドで, FragmentManagerクラスのインスタンスを取得する
+            val fragmentManager = this.supportFragmentManager
+            // FragmentTransactionクラスのインスタンスを取得し, トランザクションを開始する
+            // - commit()メソッドが実行されるまで, 実行が猶予される
+            val fragmentTransaction
+                    = fragmentManager.beginTransaction()
+            fragmentTransaction
+                .replace(R.id.container, fragment)    // フラグメントの入れ替えを行う
+                .addToBackStack(null)   // バックスタックにフラグメントを追加する (端末の戻るボタンを押すと1つ前の状態を表示できる)
+                .commit()   // フラグメントへの操作を確定する
+        }
+
+        // 「カバ」ボタンのタップイベントを監視する
+        hippoButton.setOnClickListener {
+            val fragment = HippoFragment()
+            val fragmentManager = this.supportFragmentManager
+            var fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        // 「キリン」ボタンのタップイベントを監視する
+        giraffeButton.setOnClickListener {
+            val fragment = GiraffeFragment()
+            val fragmentManager = this.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
 }
