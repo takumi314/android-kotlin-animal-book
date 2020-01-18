edaef0c0012409a717637bd8fe62442e27ca2e8b
takumi314 <isospin_symmetry@me.com>
2020-01-19 00:22:33 +0900

    add setTitle method for Fragment

--- app/src/main/java/com/example/animalbook/TitleFragment.kt
+++ app/src/main/java/com/example/animalbook/TitleFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_title.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +14,17 @@ import android.view.ViewGroup
 class TitleFragment : Fragment() {
 
     // フラグメントが初めてUIを描画するタイミングでコールされる
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+
+    fun setTitle(title: String) {
+        titleText.text = title
     }
 
 
