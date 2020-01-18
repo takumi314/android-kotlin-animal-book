9fec8f4f11006bb6b5f23ccd6d71dd2fcea90b46
takumi314 <isospin_symmetry@me.com>
2020-01-19 00:16:20 +0900

    layouts TitleText on Fragment

--- app/src/main/java/com/example/animalbook/TitleFragment.kt
+++ app/src/main/java/com/example/animalbook/TitleFragment.kt
@@ -12,7 +12,9 @@ import android.view.ViewGroup
  */
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    // フラグメントが初めてUIを描画するタイミングでコールされる
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_title, container, false)
