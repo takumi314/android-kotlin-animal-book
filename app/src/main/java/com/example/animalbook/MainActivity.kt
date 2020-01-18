cbd15bd171be201de33e22b00c458242d4035772
takumi314 <isospin_symmetry@me.com>
2020-01-19 01:02:50 +0900

    set Title on MainActivy

--- app/src/main/java/com/example/animalbook/MainActivity.kt
+++ app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = titleFragment as? TitleFragment
+        fragment?.setTitle("フラグメント動物図鑑")
     }
+
 }
